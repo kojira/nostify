d281f5e00c6234e6081e84138bbb1bf9970d02f7
Akio Kondo <kojiran@gmail.com>
2023-03-12 20:05:20 +0900

    ✨リレー受信をrustに変更

---
+++ subscriber/src/db.rs
@@ -0,0 +1,200 @@
+use chrono::Utc;
+use mysql::{params, prelude::*, Opts, OptsBuilder};
+use nostr_sdk::Event;
+use r2d2_mysql::{mysql::Error as MysqlError, MySqlConnectionManager};
+use serde_json::json;
+use std::env;
+use std::primitive::str;
+use thiserror::Error;
+
+const DATABASE_USER: &str = "MYSQL_USER";
+const DATABASE_PASS: &str = "MYSQL_PASSWORD";
+const DATABASE_NAME: &str = "MYSQL_DATABASE";
+
+const DATABASE_POOL_SIZE: u32 = 4;
+
+fn env_var(name: &str, def_var: Option<String>) -> String {
+    let env_var = env::var(name);
+    return match def_var {
+        Some(v) => env_var.unwrap_or(v),
+        _ => env_var.expect(format!("{} must be set", name).as_str()),
+    };
+}
+
+pub(crate) fn connect() -> Result<r2d2::Pool<MySqlConnectionManager>, MysqlError> {
+    let db_user = env_var(DATABASE_USER, None);
+    let db_pass = env_var(DATABASE_PASS, None);
+    let db_name = env_var(DATABASE_NAME, None);
+    let db_url = format!(
+        "mysql://{user}:{pass}@{host}:{port}/{name}",
+        user = db_user,
+        pass = db_pass,
+        host = "db",
+        port = "3306",
+        name = db_name
+    );
+    println!("db connect");
+    let opts = Opts::from_url(&db_url).unwrap();
+    let builder = OptsBuilder::from_opts(opts);
+    let manager = MySqlConnectionManager::new(builder);
+    println!("db connect2");
+
+    // マルチスレッドでプールからコネクションを取り出すような使い方を想定してArcでラップします
+    let pool = r2d2::Pool::builder()
+        .max_size(DATABASE_POOL_SIZE)
+        .build(manager)
+        .unwrap();
+    println!("db connect3");
+    Ok(pool)
+}
+
+pub fn insert_event(pool: &r2d2::Pool<MySqlConnectionManager>, event: &Event) -> () {
+    let pool = pool.clone();
+    let mut conn = pool.get().unwrap();
+    let event_id = event.id.to_hex();
+    let event_created_at = event.created_at.as_u64();
+    let received_at = Utc::now().timestamp();
+    let status = 0;
+    let tags_json = json!(event.tags).to_string();
+    let param = params! {
+        "status" => status,
+        "hex_event_id" => event_id,
+        "pubkey" => event.pubkey.to_string(),
+        "kind" => event.kind.as_u64(),
+        "content" => event.content.to_string(),
+        "tags" => tags_json,
+        "signature" => event.sig.to_string(),
+        "event_created_at" => event_created_at,
+        "received_at" => received_at
+    };
+
+    conn.exec_drop(
+        "INSERT IGNORE INTO events (status, hex_event_id, pubkey, kind, content, tags, signature, event_created_at, received_at)
+         VALUES (:status, :hex_event_id, :pubkey, :kind, :content, :tags, :signature, FROM_UNIXTIME(:event_created_at), FROM_UNIXTIME(:received_at))",
+         param,
+    ).unwrap();
+}
+
+struct NgWord {
+    // id: i64,
+    // status: i64,
+    word: String,
+    // created_at: Option<String>,
+}
+
+pub fn is_matching_ng_word(
+    pool: &r2d2::Pool<MySqlConnectionManager>,
+    search_word: &str,
+) -> Result<bool, mysql::Error> {
+    let ng_words = get_ng_words(pool)?;
+    for ng_word in ng_words {
+        if search_word.contains(&ng_word.word) {
+            return Ok(true);
+        }
+    }
+    Ok(false)
+}
+
+fn get_ng_words(pool: &r2d2::Pool<MySqlConnectionManager>) -> Result<Vec<NgWord>, mysql::Error> {
+    let pool = pool.clone();
+    let mut conn = pool.get().unwrap();
+    let ng_words_iter = conn
+        .exec_iter(
+            "SELECT id, status, word, created_at FROM ng_words WHERE status = 0",
+            (),
+        )
+        .unwrap();
+    let ng_words: Vec<NgWord> = ng_words_iter
+        .map(|row| {
+            let r = row.unwrap();
+            NgWord {
+                // id: r.get("id").unwrap(),
+                // status: r.get("status").unwrap(),
+                word: r.get("word").unwrap(),
+                // created_at: r.get("created_at").unwrap(),
+            }
+        })
+        .collect();
+    Ok(ng_words)
+}
+
+#[derive(Debug)]
+pub struct Filter {
+    pub target_channel_id: i64,
+    pub pubkeys: Option<String>,
+    pub kinds: Option<String>,
+    pub authors: Option<String>,
+    pub since: Option<i32>,
+    pub until: Option<i32>,
+    pub event_refs: Option<String>,
+    pub pubkey_refs: Option<i32>,
+    pub keywords: Option<String>,
+}
+
+pub fn get_filters(pool: &r2d2::Pool<MySqlConnectionManager>) -> Result<Vec<Filter>, MysqlError> {
+    let pool = pool.clone();
+    let mut conn = pool.get().unwrap();
+    let filters_iter = conn.exec_iter("SELECT target_channel_id, pubkeys, kinds, authors, since, until, event_refs, pubkey_refs, keywords FROM filters WHERE status = 0",())?;
+
+    let filters: Vec<Filter> = filters_iter
+        .map(|row| {
+            let r = row.unwrap();
+            Filter {
+                target_channel_id: r.get("target_channel_id").unwrap(),
+                pubkeys: r.get("pubkeys").unwrap(),
+                kinds: r.get("kinds").unwrap(),
+                authors: r.get("authors").unwrap(),
+                since: r.get("since").unwrap(),
+                until: r.get("until").unwrap(),
+                event_refs: r.get("event_refs").unwrap(),
+                pubkey_refs: r.get("pubkey_refs").unwrap(),
+                keywords: r.get("keywords").unwrap(),
+            }
+        })
+        .collect();
+    Ok(filters)
+}
+
+#[derive(Error, Debug)]
+pub enum NotifyQueueInsertError {
+    #[error("Failed to get a database connection")]
+    ConnectionFailed(#[from] r2d2::Error),
+    #[error("Failed to insert row into notify_queue table")]
+    InsertionFailed(#[from] MysqlError),
+}
+
+pub fn insert_notify_queue(
+    pool: &r2d2::Pool<MySqlConnectionManager>,
+    hex_event_id: &str,
+    target_channel_id: i64,
+) -> Result<(), NotifyQueueInsertError> {
+    let mut conn = pool.get()?;
+
+    let query = "
+        INSERT INTO notify_queue (
+            hex_event_id,
+            status,
+            target_channel_id,
+            error_count,
+            created_at
+        ) VALUES (
+            :hex_event_id,
+            :status,
+            :target_channel_id,
+            :error_count,
+            FROM_UNIXTIME(:created_at)
+        )
+    ";
+
+    let params = params! {
+        "hex_event_id" => hex_event_id,
+        "status" => 0,
+        "target_channel_id" => target_channel_id,
+        "error_count" => 0,
+        "created_at" => Utc::now().timestamp(),
+    };
+
+    conn.exec_drop(query, params)?;
+
+    Ok(())
+}
---
+++ subscriber/src/main.rs
@@ -0,0 +1,113 @@
+mod db;
+use nostr_sdk::client::blocking::Client;
+use nostr_sdk::prelude::*;
+use serde::{Deserialize, Serialize};
+
+use std::fs::File;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    relay_servers: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    // let my_keys =
+    //     Keys::from_sk_str("e706bd0c6ed2bdcc5c9a9696a794e2b0662dff551a8b933fe6232e6c4c9df655")?;
+    let my_keys: Keys = Keys::generate();
+
+    // Show bech32 public key
+    let bech32_pubkey: String = my_keys.public_key().to_bech32()?;
+    println!("Bech32 PubKey: {}", bech32_pubkey);
+
+    let file = File::open("../common/config.yml")?;
+    let config: Config = serde_yaml::from_reader(file)?;
+    let pool = db::connect()?;
+    println!("mysql pool connected");
+
+    // Create new client
+    let client = Client::new(&my_keys);
+    for item in config.relay_servers.iter() {
+        client.add_relay(item, None)?;
+    }
+    println!("add_relay");
+
+    // Connect to relays
+    client.connect();
+    println!("client.connect");
+
+    let subscription = Filter::new()
+        .kinds([nostr_sdk::Kind::Metadata, nostr_sdk::Kind::TextNote].to_vec())
+        .since(Timestamp::now());
+
+    client.subscribe(vec![subscription]);
+    println!("subscribe");
+
+    // Handle notifications
+    client.handle_notifications(|notification| {
+        if let RelayPoolNotification::Event(_url, event) = notification {
+            // if event.kind == Kind::TextNote {
+            {
+                let result = db::is_matching_ng_word(&pool, &event.content).unwrap();
+                if !result {
+                    // println!("New Event: {}", event.as_json());
+                    db::insert_event(&pool, &event);
+                    if event.kind == Kind::TextNote {
+                        let filters = db::get_filters(&pool).unwrap();
+                        for filter in filters.iter() {
+                            let pubkeys = match &filter.pubkeys {
+                                Some(s) => s.split(',').collect(),
+                                None => vec![],
+                            };
+                            let mut match_pub = false;
+                            let mut match_keyword = false;
+                            let mut add_queue = false;
+                            for pubkey in pubkeys.iter() {
+                                if event.pubkey.to_string().contains(&*pubkey) {
+                                    match_pub = true;
+                                    break;
+                                }
+                            }
+                            let keywords = match &filter.keywords {
+                                Some(s) => s.split('\n').collect(),
+                                None => vec![],
+                            };
+                            for keyword in keywords.iter() {
+                                if event
+                                    .content
+                                    .to_lowercase()
+                                    .contains(&keyword.to_lowercase())
+                                {
+                                    match_keyword = true;
+                                    break;
+                                }
+                            }
+                            if pubkeys.is_empty() && match_keyword {
+                                add_queue = true;
+                            } else if keywords.is_empty() && match_pub {
+                                add_queue = true;
+                            } else if pubkeys.len() >= 1
+                                && keywords.len() >= 1
+                                && match_pub
+                                && match_keyword
+                            {
+                                add_queue = true;
+                            }
+                            if add_queue {
+                                db::insert_notify_queue(
+                                    &pool,
+                                    &event.id.to_hex(),
+                                    filter.target_channel_id,
+                                )
+                                .ok();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    })?;
+
+    Ok(())
+}
